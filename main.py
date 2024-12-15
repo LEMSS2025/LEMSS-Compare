1123fff16979695fa629246259735198c68bce50
LEMSS2025 <lemss2025@gmail.com>
2024-12-15 16:17:28 +0200

    feat: Added full code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from experiment_comparison import ExperimentComparison
 
 def main():
     # Define the experiment folders to compare
-    experiment_folders = {"test": "data/bf91a4b2f8cf1a99ba21894297f05600", "exp2": "data/a6514a317834b0b96f7f5e536ff2fd25"}
+    experiment_folders = {"exp1": "data/5e2587bfad51339c606e96652bccaf4d", "exp2": "data/78ec5035a4eaa7596581a158a65396f1"}
 
     # Initialize the Compare class with the experiment folders
     compare = ExperimentComparison(experiment_folders)
@@ -11,5 +11,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# TODO: Instead of putting legend in each graph, extract it once and save it to a new png
